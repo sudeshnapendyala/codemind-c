e3ea639ec4d7f0cf749c8314f48de464f5522fbc
sudeshnapendyala <120094542+sudeshnapendyala@users.noreply.github.com>
2023-05-22 23:13:28 +0530

    #include<stdio.h> int main() {     int x,y,n;     scanf("%d%d",&x,&y);     if(x%2==0&&x!=0||x==0&&y%2==0) {  printf("YES"); } else {     printf("NO"); } }

--- Can_share_equally?.c
+++ Can_share_equally?.c
@@ -2,14 +2,13 @@
 int main()
 {
     int x,y,n;
-    scanf("%d%d%d",&x,&y);
+    scanf("%d%d",&x,&y);
     if(x%2==0&&x!=0||x==0&&y%2==0)
-    {
-        printf("YES");
-        
-    }
-    else
-    {
-        printf("NO");
-    }
+{
+ printf("YES");   
+}
+else
+{
+    printf("NO");
+}
 }
